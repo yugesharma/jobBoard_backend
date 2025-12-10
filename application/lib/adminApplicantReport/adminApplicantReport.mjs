5e2aa734f90357c9f7ca9e92daab8360191e7c3f
yugesharma <mynameisyugesh@gmail.com>
2025-12-10 18:16:01 -0500

    modification

--- application/lib/adminApplicantReport/adminApplicantReport.mjs
+++ application/lib/adminApplicantReport/adminApplicantReport.mjs
@@ -25,11 +25,15 @@ export const handler = async (event) => {
      let body
 
      try {
+          const payloadBody=typeof event.body === "string" ? JSON.parse(event.body) : event
+          const limit=Number(payloadBody.PAGE_SIZE) 
+          const offset=Number(payloadBody.offset)
           const applicants = await runQuery(
-                    'SELECT a.appId, a.appName, COUNT(DISTINCT j.jobAppId) AS jobs_applied, COUNT(CASE WHEN j.hired = 1 THEN 1 ELSE NULL END) AS jobs_accepted, COUNT(CASE WHEN j.withdrawn = 1 THEN 1 ELSE NULL END) AS jobs_withdrawn FROM Applicants AS a LEFT JOIN JobApplication AS j ON a.appId = j.jobApp_appId_FK GROUP BY a.appId')
-          
+                    'SELECT a.appId, a.appName, COUNT(DISTINCT j.jobAppId) AS jobs_applied, COUNT(CASE WHEN j.hired = 1 THEN 1 ELSE NULL END) AS jobs_accepted, COUNT(CASE WHEN j.withdrawn = 1 THEN 1 ELSE NULL END) AS jobs_withdrawn FROM Applicants AS a LEFT JOIN JobApplication AS j ON a.appId = j.jobApp_appId_FK GROUP BY a.appId ORDER BY a.appName ASC LIMIT ? OFFSET ?',[limit, offset])
+          const totalApplicants = await runQuery('SELECT COUNT(*) AS total_count FROM Applicants')
           body = {
                applicants: applicants,
+               total_count: totalApplicants[0].total_count
           }
 
      } catch (e) {
